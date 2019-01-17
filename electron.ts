3777963f8c6d64c085149cc6ac230e041aa119d1
Thomas Nordquist <thomas.nordquist@viison.com>
2019-01-17 23:27:45 +0100

    Switch electron.js to typescript

--- electron.ts
+++ electron.ts
@@ -1,49 +1,53 @@
-const { app, BrowserWindow } = require('electron')
-const { autoUpdater } = require("electron-updater")
-const log = require('electron-log');
-const { ConnectionManager, updateNotifier } = require('./backend/build/backend/src/index.js')
-const fs = require('fs')
-const path = require('path')
-require('electron-debug')({enabled: process.argv[2] === '--debug'});
+import { UpdateInfo } from './events'
+import { BrowserWindow, app } from 'electron'
+import * as path from 'path'
+import * as fs from 'fs'
 
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log')
+import { ConnectionManager, updateNotifier } from './backend/src/index'
+
+const isDebugEnabled = Boolean(process.argv.find(arg => arg === 'debug'))
+require('electron-debug')({ enabled: isDebugEnabled })
+
+autoUpdater.logger = log
+autoUpdater.logger.transports.file.level = 'info'
+log.info('App starting...')
 
 const connectionManager = new ConnectionManager()
 connectionManager.manageConnections()
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow: BrowserWindow | undefined
 
-function createWindow () {
-  const icon = path.join(__dirname, 'icon.png')
+function createWindow() {
+  const iconPath = path.join(__dirname, 'icon.png')
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 700,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
-    icon
+    icon: iconPath,
   })
 
-  console.log(icon)
+  console.log(iconPath)
   // and load the index.html of the app.
   mainWindow.loadFile('app/index.html')
 
   // Emitted when the window is closed.
-  mainWindow.on('close', function () {
+  mainWindow.on('close', () => {
     connectionManager.closeAllConnections()
   })
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = undefined
     app.quit()
   })
 }
@@ -54,8 +58,8 @@ function createWindow () {
 app.on('ready', () => {
   createWindow()
 
-  let updateInfo
-  autoUpdater.on('update-available', (info) => {
+  let updateInfo: UpdateInfo
+  autoUpdater.on('update-available', (info: UpdateInfo) => {
     console.log('there is an update')
     updateInfo = info
   })
@@ -78,7 +82,7 @@ app.on('ready', () => {
 })
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -86,7 +90,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
