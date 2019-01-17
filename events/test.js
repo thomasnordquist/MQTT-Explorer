ed0942983bf824bcc2cedd7aed7f839805a97419
Thomas Nordquist <thomas.nordquist@viison.com>
2019-01-17 23:28:17 +0100

    Remove dev code

--- events/test.js
+++
@@ -1,9 +0,0 @@
-const { EventEmitter } = require('events');
-
-const a = new EventEmitter()
-a.on('test', () => console.log('test'))
-a.on('test2', () => console.log('test2'))
-a.removeAllListeners('test')
-
-a.emit('test')
-a.emit('test2')
