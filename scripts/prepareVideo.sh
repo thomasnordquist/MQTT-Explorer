795c410a927034ba99010b4c9ca846d62d2268bb
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-02-17 23:19:13 +0100

    Fix grey start of demo gif

--- scripts/prepareVideo.sh
+++ scripts/prepareVideo.sh
@@ -4,6 +4,7 @@ cp app.mp4 original.mp4
 ffprobe -f lavfi -i "movie=app.mp4,blackdetect[out0]" -show_entries tags=lavfi.black_start,lavfi.black_end -of default=nw=1 -v quiet > ffmpeg_info
 END_OF_BLACK=`cat ffmpeg_info | grep end | head -n1 | cut -d'=' -f2`
 
+END_OF_BLACK=$[ $END_OF_BLACK + 2.2 ]
 # Trim black frames at start
 ffmpeg -ss $END_OF_BLACK -i app.mp4 app2.mp4
 mv app2.mp4 app.mp4
