ccdffbfebde4d7193c4e8a0590e784ade5023dbd
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-03-06 23:57:49 +0100

    Modify appx after it was packed

--- scripts/afterAllArtifactBuild.ts
+++ scripts/afterAllArtifactBuild.ts
@@ -2,21 +2,29 @@ import * as fs from 'fs-extra'
 import * as path from 'path'
 import { chdir } from 'process'
 import { exec } from './util'
+import * as AdmZip from 'adm-zip'
 
 export default async function (info: any) {
-  for (const snapFile of info.artifactPaths) {
-    if (/\.snap$/.test(snapFile)) {
+  for (const artifact of info.artifactPaths) {
+    if (/\.snap$/.test(artifact)) {
       const originalDir = __dirname
-      const dirname = path.dirname(snapFile)
+      const dirname = path.dirname(artifact)
       chdir(dirname)
 
-      await exec('sudo', ['unsquashfs', snapFile])
-      await fs.remove(snapFile)
+      await exec('sudo', ['unsquashfs', artifact])
+      await fs.remove(artifact)
       await exec('sudo', ['chmod', '-R', 'g-s', 'squashfs-root'])
-      await exec('sudo', ['snap', 'run', 'snapcraft', 'pack', 'squashfs-root', '--output', snapFile])
+      await exec('sudo', ['snap', 'run', 'snapcraft', 'pack', 'squashfs-root', '--output', artifact])
       await fs.remove('squashfs-root')
 
       chdir(originalDir)
+    }
+
+    if (/\.appx$/.test(artifact)) {
+      const zip = new AdmZip(artifact)
+      const data = zip.readAsText('AppxManifest.xml')
+      const newData = data.replace('uap:DefaultTile', 'uap:DefaultTile ShowName="allLogos"')
+      zip.updateFile('AppxManifest.xml', Buffer.from(newData))
     }
   }
 }
