62cbf6547e3854eed6b45fc2f61af933cb954376
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-03-25 19:21:10 +0100

    Properly support time locales

--- app/src/components/helper/DateFormatter.tsx
+++ app/src/components/helper/DateFormatter.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react'
+import * as moment from 'moment'
 
 interface Props {
   date: Date
 }
 
 class DateFormatter extends React.Component<Props, {}> {
-
   public render() {
-    return <span>{this.props.date.toLocaleDateString()} {this.props.date.toLocaleTimeString()}</span>
+    const momentObject = this.localizedMoment()
+    return <span>{momentObject.format('L')} {momentObject.format('LTS')}</span>
+  }
+
+  private getLocale() {
+    return window.navigator.language
+  }
+
+  private localizedMoment() {
+    return moment(this.props.date).locale(this.getLocale())
   }
 }
 
