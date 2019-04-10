c7b0400e84ab101083f7ae7936a232e8e7f69276
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-04-10 12:05:27 +0200

    Revert "Fix zoom key modifier for windows"
    This reverts commit 2590a701bb5e962887a435f7f971941ca657f85d.
    Test failed

--- src/MenuTemplate.ts
+++ src/MenuTemplate.ts
@@ -89,7 +89,7 @@ const viewMenu: MenuItemConstructorOptions = {
     },
     {
       label: 'Increase size',
-      accelerator: process.platform === 'darwin' ? 'CmdOrCtrl+Plus' : 'Control+Plus',
+      accelerator: 'CmdOrCtrl+Plus',
       click: () => {
         const window = BrowserWindow.getFocusedWindow()
         if (window) {
