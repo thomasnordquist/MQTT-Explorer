0f21f10c0d08e0a3a418e60c3124388d669ce335
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-04-26 14:57:12 +0200

    Fix backend tests

--- backend/src/Model/spec/EventDispatcher.spec.ts
+++ backend/src/Model/spec/EventDispatcher.spec.ts
@@ -1,11 +1,10 @@
-import 'mocha'
 import { EventDispatcher } from '../../../../events'
-
 import { expect } from 'chai'
+import 'mocha'
 
 describe('EventDispatcher', async () => {
-  it('should dispatch', async function () {
-    const dispatcher = new EventDispatcher<string, string>('me')
+  it('should dispatch', async function() {
+    const dispatcher = new EventDispatcher<string, string>()
     this.timeout(300)
 
     setTimeout(() => dispatcher.dispatch('hello'), 5)
@@ -18,8 +17,8 @@ describe('EventDispatcher', async () => {
     expect(response).to.eq('hello')
   })
 
-  it('should unsubscribe', async function () {
-    const dispatcher = new EventDispatcher<string, string>('me')
+  it('should unsubscribe', async function() {
+    const dispatcher = new EventDispatcher<string, string>()
     this.timeout(300)
     let incrementee = 0
     const callback = (msg: any) => {
