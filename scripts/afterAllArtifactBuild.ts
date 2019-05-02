4bc90393866ceb1479f3d7d049d7afea8f2599c1
Thomas Nordquist <thomas@nordquist.de>
2019-05-03 00:59:29 +0200

    Fix sed command

--- scripts/afterAllArtifactBuild.ts
+++ scripts/afterAllArtifactBuild.ts
@@ -15,7 +15,7 @@ export default async function(info: any) {
       await exec('sudo', ['chmod', '-R', 'g-s', 'squashfs-root'])
 
       // Add command line argument to disable the sandbox
-      await exec('sudo', ['sed', "-i''", 's/^exec \([^;]*\)$/exec \1 --no-sandbox/g', 'squashfs-root/command.sh'])
+      await exec('sudo', ['sed', "-i''", 's/^exec \\([^;]*\\)$/exec \\1 --no-sandbox/g', 'squashfs-root/command.sh'])
       await exec('sudo', ['snap', 'run', 'snapcraft', 'pack', 'squashfs-root', '--output', snapFile])
       await exec('sudo', ['rm', '-rf', 'squashfs-root'])
 
