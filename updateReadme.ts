223d4b55aa371e571b512890458b86e8552d74c3
Thomas Nordquist <thomas.nordquist@viison.com>
2019-05-13 12:47:04 +0200

    Update links

--- updateReadme.ts
+++ updateReadme.ts
@@ -28,7 +28,7 @@ function fileExtension(file: string): string {
 
 async function createReadme(): Promise<void> {
   const release: Release = (await axios.get('https://api.github.com/repos/thomasnordquist/mqtt-explorer/releases')).data
-    .find((r: Release) => r.tag_name === '0.0.0-v0.2.5')
+    .find((r: Release) => r.tag_name === 'v0.2.6')
 
   const linux64 = release.assets.filter(asset => /(x86_64|amd64)\.(AppImage|deb|rpm)$/.test(asset.name))
   const windowsInstaller = release.assets.find(asset => /Setup-.+\.exe$/.test(asset.name))
