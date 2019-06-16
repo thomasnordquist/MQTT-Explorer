486a3568e93fa7bb748cc48fbeae48dc1c9835b4
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-06-16 22:31:21 +0200

    Allow plotting edge-case values

--- app/src/components/Sidebar/CodeDiff/util.tsx
+++ app/src/components/Sidebar/CodeDiff/util.tsx
@@ -49,6 +49,19 @@ export function toPlottableValue(value: any): number | undefined {
   if (isNumber && !isNaN(intVal)) {
     return intVal
   }
+
+  // Convert "on" / "off" to int 1 / 0
+  if (typeof value === 'string') {
+    if (/^(on$)|(off$)/i.test(value)) {
+      return value.toLowerCase() === 'on' ? 1 : 0
+    }
+    if (/^[0-9]*,[0-9]+$/.test(value)) {
+      let parsedFloat = parseFloat(value.replace(',', '.'))
+      if (!isNaN(parsedFloat)) {
+        return parsedFloat
+      }
+    }
+  }
 }
 
 export function isPlottable(value: any) {
