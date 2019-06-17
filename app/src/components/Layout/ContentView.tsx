2296143883d4919a68cd6ce7300b620187125a3a
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-06-17 16:36:40 +0200

    Set fixed border for bottom panel

--- app/src/components/Layout/ContentView.tsx
+++ app/src/components/Layout/ContentView.tsx
@@ -46,7 +46,7 @@ function ContentView(props: Props) {
         allowResize={true}
         style={{ height: 'calc(100vh - 64px)' }}
         pane1Style={{ maxHeight: '100%' }}
-        pane2Style={{ maxWidth: '100%', overflow: 'hidden scroll' }}
+        pane2Style={{ borderTop: '1px solid #999', overflow: 'hidden scroll' }}
         onChange={setHeight}
       >
         <span>
