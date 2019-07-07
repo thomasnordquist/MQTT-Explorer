6bead5b5a6d4573d9c719ac338e34af36a621067
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-07-07 12:59:30 +0200

    Fix animation glitch

--- app/src/components/Tree/TreeNode/effects/useAnimationToIndicateTopicUpdate.tsx
+++ app/src/components/Tree/TreeNode/effects/useAnimationToIndicateTopicUpdate.tsx
@@ -9,20 +9,21 @@ export function useAnimationToIndicateTopicUpdate(
 ) {
   useEffect(() => {
     if (ref.current && shouldAnimate && Date.now() - lastUpdate < 3000 && !selected) {
+      let timeout: any
       let animationFrame = requestAnimationFrame(() => {
         ref.current && ref.current.classList.add(className)
+
+        timeout = setTimeout(
+          () =>
+            (animationFrame = requestAnimationFrame(() => {
+              ref.current && ref.current.classList.remove(className)
+            })),
+          500
+        )
       })
 
-      const timeout = setTimeout(
-        () =>
-          (animationFrame = requestAnimationFrame(() => {
-            ref.current && ref.current.classList.remove(className)
-          })),
-        500
-      )
-
       return function cleanup() {
-        clearTimeout(timeout)
+        timeout && clearTimeout(timeout)
         animationFrame && cancelAnimationFrame(animationFrame)
       }
     }
