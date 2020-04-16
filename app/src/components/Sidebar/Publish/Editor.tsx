a89d7cf62eb2510897a197584b626b6e4d6e4e9f
Thomas Nordquist <xxnerowingerxx@gmail.com>
2020-04-16 10:50:26 +0200

    Improve json editor

--- app/src/components/Sidebar/Publish/Editor.tsx
+++ app/src/components/Sidebar/Publish/Editor.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import { default as AceEditor } from 'react-ace'
 import { Theme, withTheme } from '@material-ui/core'
-import 'brace/mode/json'
-import 'brace/theme/dawn'
-import 'brace/theme/monokai'
-import 'brace/mode/xml'
-import 'brace/mode/text'
+import 'ace-builds'
+import 'ace-builds/webpack-resolver'
+import 'ace-builds/src-noconflict/mode-json'
+import 'ace-builds/src-noconflict/mode-xml'
+import 'ace-builds/src-noconflict/snippets/json'
+import 'ace-builds/src-noconflict/snippets/xml'
+import 'ace-builds/src-noconflict/mode-text'
+import 'ace-builds/src-noconflict/theme-monokai'
 import 'react-ace'
 
 function Editor(props: {
@@ -27,6 +30,10 @@ function Editor(props: {
       name="UNIQUE_ID_OF_DIV"
       width="100%"
       height="200px"
+      enableSnippets={true}
+      enableBasicAutocompletion={true}
+      enableLiveAutocompletion={true}
+      showPrintMargin={false}
       showGutter={true}
       value={props.value}
       onChange={props.onChange}
